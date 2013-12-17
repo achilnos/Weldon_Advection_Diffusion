79f8e7d730d56002a75c83c396c068e255443b3c
achilnos <ncweldon@ucdavis.edu>
2013-12-16 18:22:31 -0800

    Current Status
    The script has been completed except for a bug where the value of m is
    changed when passes through the function “time_loop().” This occurs
    even through m is not referenced in the function, so I am unsure about
    how to fix it.

--- Upwind_v1.py
+++ Upwind_v1.py
@@ -9,28 +9,54 @@ def grid_init(n,w,f):
     
 #print grid_init(100,1,lambda x: math.cos(2*math.pi*x))
 
+def time_loop(h, hh, m, timemax, a, sigma):
+    print "time loop m = " + str(m)
+    t = 0
+    dt = sigma * h / a
+    nmax = timemax / dt
+    print "nmax =", nmax
+    print "dt =", dt
+    if nmax * dt < timemax:
+        nmax = nmax + 1
+    while(t < nmax):
+        if t + dt > timemax:
+            dt = timemax - t
+        space_loop(m, h, hh)
+        t = t + dt
+    print "t =", t
+        #actual upwind calculation next line
+
 def space_loop(m, h, hh):
+    print "space loop m = " + str(m)
     i = 0
     grid = np.zeros(m)#having this vector be bigger than m allow convient boundary condition calculations
     while(i < m):
         x = i * h - hh
         f = lambda x: math.cos(2*math.pi*x)
-        grid[i] = f(x)
+        grid[i] = f(x) #index out of bounds error!
         i = i + 1
     print grid
     GRIDS.append(grid)
     oldgrid = grid
-    #time_loop(oldgrid, h, m)
     
-def grid_maker(initsize, n):
+def grid_maker(initsize, n, timemax, a, sigma):
     i = 0
     m = initsize
+    print "grid_make m = " + str(m)
     while(i < n):
         i = i + 1
         h = 1.0 / m
         hh = h / 2.0
-        space_loop(m, h, hh)
+        time_loop(m, h, hh, timemax, a, sigma)
+        #space_loop(m, h, hh)
         m = 2 * m
 
+def define_variables():    
+    initsize = input("enter number of cells in grid: ")
+    n = input("enter length of grid: ")
+    timemax = input("enter number of timesteps: ")
+    a = input("enter a value for a: ")
+    sigma = input("enter a value for sigma: ")
+    grid_maker(initsize, n, timemax, a, sigma)
 
-grid_maker(3,4)
+define_variables()
