4eb3ba5fcc76d2383f5f473877bd8ab36847d78c
achilnos <ncweldon@ucdavis.edu>
2013-12-17 12:22:54 -0800

    Fixed several bugs
    The bug where the value of m is changed when it is passed through the
    time_loop() function was fixed. It turned out to be due to Python
    passing varibles based on order of listing, not by name. I switch the
    order of the variables and this caused m to change value with h. I also
    renamed m initsize for clairity. A new bug came up in the time_loop()
    where the while loop in it never stopped. This was because the if
    statement reset the value of dt to zero at the last timestep. As a
    temporary fix, the script breaks when that happens now.

--- Upwind_v1.py
+++ Upwind_v1.py
@@ -3,37 +3,46 @@ import math
 
 GRIDS = []
 
+#cd ~/Documents/Python/Weldon_Advection_Diffusion
+
 #w is the width of the cosine;n is resolution;f is the initial condition
 def grid_init(n,w,f):
     return np.array([f(1.0*x*w/n+w/(2.0*n)) for x in range(0, n)])
     
 #print grid_init(100,1,lambda x: math.cos(2*math.pi*x))
 
-def time_loop(h, hh, m, timemax, a, sigma):
-    print "time loop m = " + str(m)
+def time_loop(initsize, h, hh, timemax, a, sigma):
     t = 0
     dt = sigma * h / a
+    print "time_loop dt = ", dt
+    print "time_loop h = ", h
+    print "time_loop a = ", a
+    print "time_loop sigma = ", sigma
     nmax = timemax / dt
-    print "nmax =", nmax
-    print "dt =", dt
+    #print "nmax =", nmax
+    #print "dt =", dt
     if nmax * dt < timemax:
         nmax = nmax + 1
-    while(t < nmax):
+    while(t < nmax):#this loop continues indefinitly
         if t + dt > timemax:
             dt = timemax - t
-        space_loop(m, h, hh)
-        t = t + dt
-    print "t =", t
+            if dt == 0:
+                break#this could be the place to call the data comparison function
+        space_loop(initsize, h, hh)
+        print "innerloop dt = ", dt
+        t = t + dt#the problem is dt = 0 here
+        #print "t = ", t
+    #print "t =", t
         #actual upwind calculation next line
 
-def space_loop(m, h, hh):
-    print "space loop m = " + str(m)
+def space_loop(initsize, h, hh):
+    print "space loop initsize = " + str(initsize)
+    grid = np.zeros(initsize)#having this vector be bigger than m allow convient boundary condition calculations
     i = 0
-    grid = np.zeros(m)#having this vector be bigger than m allow convient boundary condition calculations
-    while(i < m):
+    while(i < initsize):#this loop exits correctly!
         x = i * h - hh
         f = lambda x: math.cos(2*math.pi*x)
-        grid[i] = f(x) #index out of bounds error!
+        grid[i] = f(x)
         i = i + 1
     print grid
     GRIDS.append(grid)
@@ -41,15 +50,13 @@ def space_loop(m, h, hh):
     
 def grid_maker(initsize, n, timemax, a, sigma):
     i = 0
-    m = initsize
-    print "grid_make m = " + str(m)
+    print "grid_make initsize = " + str(initsize)
     while(i < n):
         i = i + 1
-        h = 1.0 / m
+        h = 1.0 / initsize
         hh = h / 2.0
-        time_loop(m, h, hh, timemax, a, sigma)
-        #space_loop(m, h, hh)
-        m = 2 * m
+        time_loop(initsize, h, hh, timemax, a, sigma)
+        initsize = 2 * initsize
 
 def define_variables():    
     initsize = input("enter number of cells in grid: ")
